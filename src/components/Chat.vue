05548da113818091a55562ed32a62ba49d9a8318
Herverson <herverson.sousa@gmail.com>
2019-05-09 18:46:54 -0300

    Chat funcionando com a data e scroll

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,11 +3,11 @@
     <h2 class="center cyan-text">Chat Smile</h2>
     <div class="card lime accent-2">
       <div class="card-content">
-        <ul class="messages">
-          <li>
-            <span class="cyan-text">Nome</span>
-            <span class="grey-text text-darken-1">Mensagem</span>
-            <span class="grey-text time">Tempo</span>
+        <ul class="messages" v-chat-scroll>
+          <li v-for="mensagem in mensagens" :key="mensagem.id">
+            <span class="cyan-text">{{ mensagem.nome }}</span>
+            <span class="grey-text text-darken-1">{{ mensagem.content }}</span>
+            <span class="grey-text time">{{ mensagem.timestamps }}</span>
           </li>
         </ul>
       </div>
@@ -21,6 +21,8 @@
 <script>
 
 import NovaMensagem from '@/components/NovaMensagem'
+import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
   name: 'Chat',
@@ -30,8 +32,27 @@ export default {
   },
   data () {
     return {
-
+      mensagens: []
     }
+  },
+  created () {
+    let ref = db.collection('mensagens')
+
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type == 'added' )
+        {
+          let doc = change.doc
+          this.mensagens.push({
+            id: doc.id,
+            nome: doc.data().nome,
+            content: doc.data().content,
+            timestamps: moment(doc.data().timestamps).locale('pt-br').format('LLL')
+          })
+        }
+      });
+        
+    })
   }
 }
 </script>
@@ -47,6 +68,23 @@ export default {
 
 .chat .time {
   display: block;
-  font-size: 1.2em;
+  font-size: 0.8em;
+}
+
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
+
+.messages::-webkit-scrollbar {
+  width: 3px;
+}
+
+.messages::-webkit-scrollbar-track {
+  background-color: #ddd;
+}
+
+.messages::-webkit-scrollbar-thumb {
+  background: #aaa;
 }
 </style>
